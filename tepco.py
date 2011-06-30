7e6080ad3371dd7682c4572119c4aa74c60f2a26
Shigeru KANEMOTO <sgk@accense.com>
2011-07-01 00:07:22 +0900

    7月1日、データ形式変更に対応。

--- tepco.py
+++ tepco.py
@@ -18,8 +18,8 @@ CSV_URL = 'http://www.tepco.co.jp/forecast/html/images/juyo-j.csv'
 COLOR_BLACK = (0, 0, 0)
 COLOR_ORANGE = (255, 128, 0)
 
-def from_web(oldlastmodstr=None):
-  csv = urllib2.urlopen(CSV_URL)
+def from_web(url, oldlastmodstr=None):
+  csv = urllib2.urlopen(url or CSV_URL)
   lastmodstr = csv.headers['last-modified']
   if lastmodstr == oldlastmodstr:
     return None
@@ -33,10 +33,10 @@ def from_web(oldlastmodstr=None):
   # Power usage
   #
   usage = {}
-  line = csv.readline()		# 2011/3/26 1:30 UPDATE
+  line = csv.readline()		# 2011/6/27 23:55 UPDATE
 
-  line = csv.readline()		# ピーク時供給力(万kW),時台,供給力情報更新日,供給力情報更新時刻
-  line = csv.readline()		# 3750,18:00,3/26,1:05
+  line = csv.readline()		# ピーク時供給力（万ｋW),時間帯,供給力情報更新日,供給力情報更新時刻
+  line = csv.readline()		# 4580,14:00～15:00,6/26,17:30
   line = line.strip()
   line = line.split(',')
   r['capacity'] = int(line[0])
@@ -51,7 +51,7 @@ def from_web(oldlastmodstr=None):
 
   line = csv.readline()		# 予想最大電力(万kW),時間帯,予想最大電力情報更新日,予想最大電力情報更新時刻
 
-  line = csv.readline()		# 3350,18:00～19:00,4/8,1:05
+  line = csv.readline()		# 3640,14:00～15:00,6/26,17:30
   line = line.strip()
   line = line.split(',')
   r['forecast-peak-usage'] = int(line[0])
@@ -64,54 +64,57 @@ def from_web(oldlastmodstr=None):
   r['forecast-peak-updated'] = t
   line = csv.readline()		# empty line
 
-  line = csv.readline()		# DATE,TIME,当日実績(万kW),前日実績(万kW)
+  line = csv.readline()		# DATE,TIME,当日実績(万kW),予測値(万kW)
 
-  for line in csv:
+  for x in range(24):
+    line = csv.readline()
     line = line.strip()
     line = line.split(',')
-    power = int(line[2])
-    if power == 0:
-      break
     hour = int(line[1].split(':')[0])
-    usage[hour] = (power, False)
+    power = int(line[2])
+    forecast = int(line[3])
+    usage[hour] = (power, False, forecast)
 
-  #
-  # Rolling blackout
-  #
-  try:
-    gif = urllib2.urlopen(IMAGE_URL)
-  except:
-    pass
-  else:
-    gif = pygif.GifDecoder(gif.read())
-    image = gif.images[0]
+  if False:
+    #
+    # Rolling blackout
+    #
+    try:
+      gif = urllib2.urlopen(IMAGE_URL)
+    except:
+      pass
+    else:
+      gif = pygif.GifDecoder(gif.read())
+      image = gif.images[0]
 
-    def is_pixel(x, y, col):
-      pix = image.pixels[y * image.width + x]
-      pix = gif.pallete[pix]
-      d = (
-	(pix[0] - col[0]) ** 2 +
-	(pix[1] - col[1]) ** 2 +
-	(pix[2] - col[2]) ** 2
-      )
-      return (d < 100)
+      def is_pixel(x, y, col):
+	pix = image.pixels[y * image.width + x]
+	pix = gif.pallete[pix]
+	d = (
+	  (pix[0] - col[0]) ** 2 +
+	  (pix[1] - col[1]) ** 2 +
+	  (pix[2] - col[2]) ** 2
+	)
+	return (d < 100)
 
-    def comb():
-      for x in range(50, 670):
-	if is_pixel(x, 387, COLOR_BLACK):
-	  yield x + 1
+      def comb():
+	for x in range(50, 670):
+	  if is_pixel(x, 387, COLOR_BLACK):
+	    yield x + 1
 
-    for h, x in zip(usage.iterkeys(), comb()):
-      usage[h] = (usage[h][0], is_pixel(x, 387, COLOR_ORANGE))
+      for h, x in zip(usage.iterkeys(), comb()):
+	usage[h] = (usage[h][0], is_pixel(x, 387, COLOR_ORANGE))
 
   r['usage'] = usage
   return r
 
 def main():
   import locale
-  locale.setlocale(locale.LC_ALL, 'C')
   import pprint
-  pprint.pprint(from_web())
+  import sys
+  locale.setlocale(locale.LC_ALL, 'C')
+  url = sys.argv[1] if len(sys.argv) > 1 else None
+  pprint.pprint(from_web(url))
 
 if __name__ == '__main__':
   main()
